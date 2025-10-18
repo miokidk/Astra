2636cb2c48885eccec5455c41cf7a40131690e1a
Khalid Jackson <khalid.dev@icloud.com>
2025-10-18 12:59:36 -0400

    Initial Commit

---
+++ Astra/AstraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstraApp.swift
+//  Astra
+//
+//  Created by Khalid Jackson on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AstraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
